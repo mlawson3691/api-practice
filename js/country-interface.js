566a09ba5b140b1403bd0990ede1c079c21b586b
Katy Henning and Mark Lawson <mark@>
2016-10-13 09:59:56 -0700

    generate a random country and display to user

---
+++ js/country-interface.js
@@ -0,0 +1,8 @@
+var Country = require('./../js/country.js').countryModule;
+
+$(document).ready(function() {
+  var country = new Country();
+  $('#start').click(function() {
+    $('#country').text(country.randomCountry());
+  });
+});
--- js/map-interface.js
+++ js/map-interface.js
@@ -1,10 +1,12 @@
 var apiKey = require('./../.env').apiKey;
-
 var Map = require('./../js/map.js').mapModule;
 
+$('head').append('<script src=\"https://maps.googleapis.com/maps/api/js?key=' + apiKey + '\" async defer></script>');
+
 $(document).ready(function() {
-  $('head').append('<script src=\"https://maps.googleapis.com/maps/api/js?key=' + apiKey + '\" async defer></script>');
+
   var map = new Map();
   map.locateUser();
 
+  
 });
